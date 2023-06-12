d74b4fc1cc7d1a4867cbdc3433bf0d2cd444633b
Meher Tasnim <mehertasnim95@gmail.com>
2023-06-12 09:34:25 -0500

    added starter file, initialized spring boot application

---
+++ src/test/java/com/mehertasnim/Movie/Theater/API/MovieTheaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehertasnim.Movie.Theater.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTheaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
