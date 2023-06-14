42f72ee6a50bec69792bfbcb95fd61d20b085b9b
Meher Tasnim <mehertasnim95@gmail.com>
2023-06-14 09:55:00 -0500

    added user model

---
+++ src/main/java/com/mehertasnim/Movie/Theater/API/model/User.java
@@ -0,0 +1,49 @@
+package com.mehertasnim.Movie.Theater.API.model;
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "username", nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public User(Long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
